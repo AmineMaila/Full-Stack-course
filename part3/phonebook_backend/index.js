59eae8052a11352ac4a2d5218cb5f68aa3c76b40
AmineMaila <aminemaila18@gmail.com>
2025-05-03 14:24:45 +0100

    Deployed to production

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -3,6 +3,8 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('dist'))
+
 
 morgan.token('body', (req, res) => {
 	return (JSON.stringify(req.body))
@@ -87,7 +89,7 @@ app.delete('/api/persons/:id', (req, res) => {
 		res.status(404).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
 	console.log("Listing on port ", PORT)
