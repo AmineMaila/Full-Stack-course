a9925ba590bfaf5ed7bb87c05a4d4cda782c74b9
AmineMaila <aminemaila18@gmail.com>
2025-05-05 13:22:46 +0100

    3.13: Phonebook database, step 1

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const Person = require('./models/person')
 const app = express()
 
 app.use(express.json())
@@ -12,31 +14,12 @@ morgan.token('body', (req, res) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-const people = [
-	{ 
-		"id": "1",
-		"name": "Arto Hellas", 
-		"number": "040-123456"
-	},
-	{ 
-		"id": "2",
-		"name": "Ada Lovelace",
-		"number": "39-44-5323523"
-	},
-	{ 
-		"id": "3",
-		"name": "Dan Abramov", 
-		"number": "12-43-234345"
-	},
-	{ 
-		"id": "4",
-		"name": "Mary Poppendieck", 
-		"number": "39-23-6423122"
-	}
-]
-
 app.get('/api/persons', (req, res) => {
-	res.json(people)
+	Person.find({})
+		.then(people => {
+			res.json(people)
+		})
+		.catch(error => {res.status(500).end(JSON.stringify({error: error.message}))})
 })
 
 app.get('/info', (req, res) => {
---
+++ part3/phonebook_backend/models/person.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+
+const uri = process.env.MONGODB_URI
+const PORT = process.env.PORT
+
+const MAX_RETRIES = 3
+const RETRY_DELAY = 3000
+let retries = 0
+
+const connectWithRetries = () => {
+	mongoose.connect(uri)
+		.then(() => {
+			console.log('Connected to MongoDB')
+			retries = 0
+		})
+		.catch(err => {
+			console.log('MongoDB connection failed: ', err)
+			if (retries < MAX_RETRIES) {
+				retries++;
+				console.log('Retrying...')
+				setTimeout(connectWithRetries, RETRY_DELAY)
+			} else {
+				console.log('Max retries reached. exiting...')
+				process.exit(1)
+			}
+		})
+}
+
+mongoose.connection.on('error', err => {
+	console.log('MongoDB error: ', err)
+})
+
+mongoose.connection.on('disconnected', () => {
+	console.log('MongoDB connection lost. Retrying...')
+	if (retries < MAX_RETRIES) {
+		setTimeout(connectWithRetries, RETRY_DELAY)
+	} else {
+		console.log('Max retries reached. exiting...')
+		process.exit(1)
+	}
+})
+
+connectWithRetries()
+
+mongoose.set('strictQuery', false) // when set to false means you can query documents that don't follow the schema
+
+const personSchema = new mongoose.Schema({
+	name: String,
+	number: String,
+})
+
+module.exports = mongoose.model('Person', personSchema)
+
+
