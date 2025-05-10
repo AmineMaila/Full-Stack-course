e451d49e4cf01645d3163c68bb5ff8e4df7f9336
AmineMaila <aminemaila18@gmail.com>
2025-05-10 10:47:19 +0100

    4.10: Blog List Tests, step 3

--- part4/blog/tests/super.test.js
+++ part4/blog/tests/super.test.js
@@ -27,6 +27,32 @@ test('returned blog has property id not _id', async () => {
 	assert(response.body[0].hasOwnProperty('id') && !response.body[0].hasOwnProperty('_id'))
 })
 
+test('blog created successfully to database', async () => {
+	const blog = {
+		title: "HTTP cycle",
+		author: "AmineMaila",
+		url: "https://pine-meat-103.notion.site/Walkthrough-of-HTTPS-request-and-response-14abc3b09421804e92b3f2f4764c97c6?pvs=4",
+		likes: 0
+	}
+
+	const response = await api.post('/api/blogs')
+		.send(blog)
+		.expect(201)
+		.expect('Content-Type', /application\/json/)
+
+	assert.deepStrictEqual({
+		title: response.body.title,
+		author: response.body.author,
+		url: response.body.url,
+		likes: response.body.likes,
+	},
+	blog)
+
+	const blogs = await api.get('/api/blogs')
+		.expect(200)
+	assert.strictEqual(blogs.body.length, initialBlogs.length + 1)
+})
+
 after(() => {
 	mongoose.connection.close()
 })
